a3862f4869e368d73b41f972293099d7c47c4170
Yadamzer <yadamzerterefe09@gmail.com>
2024-12-02 03:13:43 +0300

    Task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,12 @@
+/**
+ * log msg to the console
+ * @param {string} msg 
+ */
 const displayMessage = (msg) => {
     console.log(msg);
 };
 
+/**
+ * export the function
+ */
 module.exports = displayMessage;
