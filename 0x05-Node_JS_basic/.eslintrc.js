a050944a44da2161441dc45cb81fec53cddc7b2a
Yadamzer <yadamzerterefe09@gmail.com>
2024-12-07 11:28:48 +0300

    Task 0

--- 0x05-Node_JS_basic/.eslintrc.js
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -1,37 +1,17 @@
 module.exports = {
-    env: {
-        browser: false,
-        es6: true,
-        jest: true,
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
     },
-    extends: [
-        'airbnb-base',
-        'plugin:jest/all',
-    ],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
+    "parserOptions": {
+        "ecmaVersion": 2018
     },
-    plugins: ['jest'],
-    rules: {
-        'max-classes-per-file': 'off',
-        'no-underscore-dangle': 'off',
-        'no-console': 'off',
-        'no-shadow': 'off',
-        'no-restricted-syntax': [
-            'error',
-            'LabeledStatement',
-            'WithStatement',
-        ],
-    },
-    overrides: [
-        {
-            files: ['*.js'],
-            excludedFiles: 'babel.config.js',
-        }
-    ]
+    "rules": {
+    }
 };
