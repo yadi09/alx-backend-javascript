1232693450b6f313af77e14d20a697800c7c27b0
Yadamzer <yadamzerterefe09@gmail.com>
2024-12-07 23:31:28 +0300

    Task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,11 +3,11 @@ const express = require('express');
 const { readFile } = require('fs');
 
 const app = express();
-const PORT = 1245;
+const port = 1245;
 
 function countStudents(fileName) {
-  const stds = {};
-  const flds = {};
+  const students = {};
+  const fields = {};
   let length = 0;
   return new Promise((resolve, reject) => {
     readFile(fileName, (err, data) => {
@@ -19,25 +19,25 @@ function countStudents(fileName) {
         for (let i = 0; i < lines.length; i += 1) {
           if (lines[i]) {
             length += 1;
-            const fld = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(stds, fld[3])) {
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
               students[field[3]].push(field[0]);
             } else {
               students[field[3]] = [field[0]];
             }
-            if (Object.prototype.hasOwnProperty.call(flds, field[3])) {
-              flds[field[3]] += 1;
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
             } else {
-              flds[field[3]] = 1;
+              fields[field[3]] = 1;
             }
           }
         }
         const l = length - 1;
         output += `Number of students: ${l}\n`;
-        for (const [key, value] of Object.entries(flds)) {
+        for (const [key, value] of Object.entries(fields)) {
           if (key !== 'field') {
             output += `Number of students in ${key}: ${value}. `;
-            output += `List: ${stds[key].join(', ')}\n`;
+            output += `List: ${students[key].join(', ')}\n`;
           }
         }
         resolve(output);
@@ -57,7 +57,7 @@ app.get('/students', (request, response) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(port, () => {
 });
 
 module.exports = app;
