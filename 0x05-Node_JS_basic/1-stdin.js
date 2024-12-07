46c5cb088bd40c9a93c38be77931ad3ec88242af
Yadamzer <yadamzerterefe09@gmail.com>
2024-12-07 22:39:41 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,7 @@
-process.stdin.setEncoding('utf8');
-
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const name = data.trim(); // Remove any leading/trailing whitespace
-    process.stdout.write(`Your name is: ${name}\n`); // Display the input name
+    process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
