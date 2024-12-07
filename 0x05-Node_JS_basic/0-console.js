5e61af7144245ba34f0fee1dae1bd30f046e8216
Yadamzer <yadamzerterefe09@gmail.com>
2024-12-07 11:19:52 +0300

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * msg to the console.
+ * @param {string} message - msg
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (msg) => console.log(msg);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
